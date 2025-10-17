a4c130810e9a2385e99db4bf5d1c50cbbfc01e47
Alexander Rapp <alexander@alexander.co.tz>
2025-10-17 19:04:15 +0200

    transportosm.sh bugfix

--- scripts/transportosm.sh
+++ scripts/transportosm.sh
@@ -10,11 +10,11 @@ for i in `seq $1 $2`; do
     mkdir -p $i; cd $i
     for j in `seq $3 $4`; do
         if [ ! -f $j.png ]; then
-            echo https://a.tile.thunderforest.com/transport/${zoom}/${i}/${j}.png?${TFORESTKEY}
+            echo https://a.tile.thunderforest.com/transport/${zoom}/${i}/${j}.png?apikey=${TFORESTKEY}
         fi
     done | xargs wget -T 60 -c; rename s/.apikey=.*// *.png*; cd ..
 done
 for i in `seq $1 $2`; do
-    montage -mode Concatenate -tile 1x`expr $4 - $3 + 1` `seq $3 $4 | sed -e's/$/.png/' | sed -e"s!^!$i/!"` $i.png
+    montage -mode Concatenate -tile 1x$(($4 - $3 + 1)) `seq $3 $4 | sed -e"s/$/.png/; s!^!$i/!"` $i.png
 done
-montage -mode Concatenate -tile `expr $2 - $1 + 1`x1 `seq $1 $2 | sed -e's/$/.png/'` osm.png
+montage -mode Concatenate -tile $(($2 - $1 + 1))x1 `seq $1 $2 | sed -e's/$/.png/'` osm.png
