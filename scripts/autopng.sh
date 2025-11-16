aa45c314f01b9ec840b498235a9367e1cd4798ad
Alexander Rapp <alexander@alexander.co.tz>
2025-11-16 14:54:50 +0100

    continue hosting experiments and fantasymaps on alexander.co.tz for now

--- scripts/autopng.sh
+++ scripts/autopng.sh
@@ -2,8 +2,7 @@
 for i in $@; do
     b=`basename $i .svg`
     if [ $i -nt ${b}.png ]; then
-        W=$(awk "BEGIN{print int(0.5+$(grep '  width=' $i | head -n1| sed -e's/"$//; s/.*"//;')*30/138)}")
-        inkscape -b ffffff -y 255 -d 20.875 -o ${b}-24bit.png ${b}.svg
+        inkscape -b ffffff -y 255 -d 20.87 -o ${b}-24bit.png ${b}.svg
         convert ${b}-24bit.png -type palette PNG8:${b}.png
         rm ${b}-24bit.png
     fi
